485a37e72795d430229cc799e34d02b2861cf5b9
Julien Look <julien.look@cinoware.com>
2024-09-25 11:20:20 +0200

    .

--- Woche 2/Tag3/bs4_intro.py
+++ Woche 2/Tag3/bs4_intro.py
@@ -7,7 +7,9 @@ response = requests.get(BASE_URL)
 
 """ Auftrag ist: die Stellenangebote aus der Webseite zu extrahieren. 
     1. Extrahiere die Titel der Jobangebote
-    2. ...
+    2. Untertitel
+    3. Ort
+    4. Datum
 """
 
 
@@ -15,6 +17,20 @@ response = requests.get(BASE_URL)
 soup = BeautifulSoup(response.text, "html.parser")
 
 titles = soup.find_all("h2", class_="title is-5")
-print("Anzahl der Stellenangebote: ", len(titles))
 for title in titles:
     print(title.text)
+
+# 2. Extrahiere die Untertitel der Jobangebote
+subtitles = soup.find_all("h3", class_="subtitle is-6 company")
+for subtitle in subtitles:
+    print(subtitle.text)
+
+# 3. Extrahiere die Orte der Jobangebote
+locations = soup.find_all("p", class_="location")
+for location in locations:
+    print(location.text)
+
+# 4. Extrahiere die Datum der Jobangebote
+dates = soup.find_all("time")
+for date in dates:
+    print(date.text)
