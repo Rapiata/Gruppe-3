ad82466a412bd55f5b3a87430ca376eaeca3f2f7
Julien Look <julien.look@cinoware.com>
2024-09-25 11:01:20 +0200

    .

--- Woche 2/Tag3/bs4_intro.py
+++ Woche 2/Tag3/bs4_intro.py
@@ -1,7 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
 BASE_URL = "https://realpython.github.io/fake-jobs/"
 response = requests.get(BASE_URL)
 
 
-print(response.text)
+""" Auftrag ist: die Stellenangebote aus der Webseite zu extrahieren. 
+    1. Extrahiere die Titel der Jobangebote
+    2. ...
+"""
+
+
+# Wir erstellen ein BeautifulSoup Objekt
+soup = BeautifulSoup(response.text, "html.parser")
+
+titles = soup.find_all("h2", class_="title is-5")
+for title in titles:
+    print(title.text)
