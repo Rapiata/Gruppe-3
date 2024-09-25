95eccc2060d3abb40dd9a2388c7e7522204d9a82
Julien Look <julien.look@cinoware.com>
2024-09-25 12:22:19 +0200

    .

--- Woche 2/Tag3/bs4_intro.py
+++ Woche 2/Tag3/bs4_intro.py
@@ -18,22 +18,25 @@ BASE_URL = "https://realpython.github.io/fake-jobs/"
 #         self.date = date
 
 
-def extract_jobs(soup):
+def extract_jobs(soup: BeautifulSoup) -> list:
     jobs = []
     job_elements = soup.find_all("div", class_="card")
 
     for job_element in job_elements:
         title_element = job_element.find("h2", class_="title is-5")
-        title = title_element.text
+        title = title_element.text.strip()
 
         subtitle_element = job_element.find("h3", class_="subtitle is-6 company")
-        subtitle = subtitle_element.text
+        subtitle = subtitle_element.text.strip()
 
         location_element = job_element.find("p", class_="location")
-        location = location_element.text
+        location_raw = location_element.text
+        location = (
+            location_raw.strip()
+        )  # Wir entfernen die Leerzeichen und Zeilenumbrüche am Anfang und Ende des Strings mit strip()
 
         date_element = job_element.find("time")
-        date = date_element.text
+        date = date_element.text.strip()
 
         job = {
             "title": title,
@@ -68,3 +71,15 @@ jobs = extract_jobs(soup)
 # Wir erstellen ein CSV aus den Daten
 df = pd.DataFrame([job for job in jobs])
 df.to_csv("jobs.csv", index=False)
+
+
+# Zusatz:
+# CSV im richtigen Ordner abspeichern
+
+
+# Wenn wir Jobs mit Auslaufdatum haben, wollen wir diese automatisch löschen - Wie machen wir das?
+
+# 0. Einen Server einrichten
+# 1. Eine Datenbank einrichten, die die Jobs speichert
+# 2. Einen Scheduler einrichten (einmal am Tag um 18 Uhr), der die Jobs regelmäßig abruft und prüft
+# 3. Die Jobs aus der Datenbank löschen, die abgelaufen sind
