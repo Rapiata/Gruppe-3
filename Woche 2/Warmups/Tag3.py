d9ba5595787224242243a28bd2098b596d3cb4cb
Julien Look <julien.look@cinoware.com>
2024-09-25 10:03:55 +0200

    web intro

--- Woche 2/Warmups/Tag3.py
+++ Woche 2/Warmups/Tag3.py
@@ -5,3 +5,22 @@ Gib aus den Daten eine Liste mit den Namen der Objekte zurück.
 Zusatzaufgabe:
 Gib gemeinsam mit dem Namen zusätzlich die ID der Objekte aus.
 """
+
+import requests
+
+
+def get_posts(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        posts = []
+        for post in data:
+            posts.append({"title": post["title"], "id": post["id"]})
+
+        return posts
+    else:
+        raise Exception("Error: Could not get posts")
+
+
+posts = get_posts("https://jsonplaceholder.typicode.com/posts")
+print(posts)
