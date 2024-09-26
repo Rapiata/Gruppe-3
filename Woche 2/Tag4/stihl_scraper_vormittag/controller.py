98e8f361b97a98cc7fde8c6715adf2ecf17690d1
Julien Look <julien.look@cinoware.com>
2024-09-26 10:53:24 +0200

    .

--- Woche 2/Tag4/stihl_scraper_vormittag/controller.py
+++ Woche 2/Tag4/stihl_scraper_vormittag/controller.py
@@ -1,12 +1,30 @@
-def scrape_product_categories():
+from urllib.parse import urljoin
+
+import model
+import requests
+import view
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://www.stihl.de/de/"
+
+
+def scrape_product_categories() -> list[dict]:
     # 1. request auf die startseite
-    # 2. soup erstellen
-    # 3. model aufrufen - extrahiere produkt kategorien
-    # 4. return produkt kategorien, weitergabe an zweiten schritt: scrape_products
-    pass
+    full_url = urljoin(BASE_URL, "geraete-werkzeuge")
+    response = requests.get(full_url)
+
+    if response.status_code == 200:
+        # 2. soup erstellen
+        soup = BeautifulSoup(response.text, "html.parser")
+        # 3. model aufrufen - extrahiere produkt kategorien
+        product_categories = model.extract_product_categories(soup)
+        # 4. return produkt kategorien, weitergabe an zweiten schritt: scrape_products
+        return product_categories
+    else:
+        raise Exception("Request failed, URL does not exist!")
 
 
-def scrape_products():
+def scrape_products(product_categories: list[dict]) -> None:
     # 1. request auf die dedizierte produktkategorie
     # 2. soup erstellen
     # 3. model aufrufen - extrahiere produktdetails
