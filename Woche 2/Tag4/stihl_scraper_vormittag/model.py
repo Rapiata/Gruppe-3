d0a77734df9e5b1afd3a29db453ab0398bc5b0c9
Julien Look <julien.look@cinoware.com>
2024-09-26 11:20:51 +0200

    .

--- Woche 2/Tag4/stihl_scraper_vormittag/model.py
+++ Woche 2/Tag4/stihl_scraper_vormittag/model.py
@@ -22,6 +22,7 @@ def extract_product_categories(soup: BeautifulSoup) -> list[dict]:
         product_category = ProductCategory(category=product_name, path=product_link)
         product_categories.append(product_category)
     # returns list of product categories: {'category': 'category_name', 'url': 'category_url'}
+
     return product_categories
 
 
