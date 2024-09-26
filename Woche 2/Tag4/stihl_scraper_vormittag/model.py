585d79d5385d93b53223c522aca0df18d1537617
Julien Look <julien.look@cinoware.com>
2024-09-26 11:06:59 +0200

    .

--- Woche 2/Tag4/stihl_scraper_vormittag/model.py
+++ Woche 2/Tag4/stihl_scraper_vormittag/model.py
@@ -1,7 +1,28 @@
-def extract_product_categories(soup) -> list[dict]:
+from dataclasses import dataclass
+
+from bs4 import BeautifulSoup
+
+
+@dataclass
+class ProductCategory:
+    category: str
+    path: str
+
+
+def extract_product_categories(soup: BeautifulSoup) -> list[dict]:
     # webscraping magic
+    grid = soup.find("div", class_="categorygrid grid section")
+    product_elements = grid.find_all("a")
+    product_categories = []
+
+    for product_element in product_elements:
+        product_link = product_element["href"]
+        product_name = product_element.text
+
+        product_category = ProductCategory(category=product_name, path=product_link)
+        product_categories.append(product_category)
     # returns list of product categories: {'category': 'category_name', 'url': 'category_url'}
-    pass
+    return product_categories
 
 
 def extract_product_details(soup) -> list[dict]:
