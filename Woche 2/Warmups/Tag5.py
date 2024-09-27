a1c0226398b47c641ac18bf2d53a7a41f0302cd8
Julien Look <julien.look@cinoware.com>
2024-09-27 09:01:25 +0200

    .

---
+++ Woche 2/Warmups/Tag5.py
@@ -0,0 +1,5 @@
+"""
+Scrape die Seite Python Jobs (https://pythonjobs.github.io/) und extrahiere die Job-Titel und das Erstellungsdatum.
+Erstelle eine Klasse Job, die den Titel und das Erstellungsdatum speichern.
+Gib eine Liste von Job-Objekten zurück, die auf der Seite zu finden sind
+"""
